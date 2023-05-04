1a3829c3d1a5624f1d52cba59859b57311eda8b9
Shubham Kumar <shubhamsingh98354@gmail.com>
2023-05-05 00:24:20 +0530

    sync

--- .github/scripts/authenticateorg.sh
+++ .github/scripts/authenticateorg.sh
@@ -1,3 +1,4 @@
 echo ${SECRET_URL_PATH} > ./CI_SFDX_URL.txt && 
 cat ./CI_SFDX_URL.txt
-sfdx force:auth:sfdxurl:store -f ./CI_SFDX_URL.txt -a ${SOURCE_ORG_ALIAS} -d
+sf org login sfdx-url --sfdx-url-file ./CI_SFDX_URL.txt --set-default --alias ${SOURCE_ORG_ALIAS}
+# sfdx force:auth:sfdxurl:store -f ./CI_SFDX_URL.txt -a ${SOURCE_ORG_ALIAS} -d
--- .github/scripts/generatedeltapkg.sh
+++ .github/scripts/generatedeltapkg.sh
@@ -1,8 +1,8 @@
 export HOME=/root
 git config --global --add safe.directory '*'
 git fetch origin "+refs/heads/*:refs/remotes/origin/*"
-sfdx sgd:source:delta --help
-sfdx sgd:source:delta -a 53 --to HEAD --from origin/$BASE_BRANCH --output . -i manifest/mdapi/.sgdignore
+sf sgd:source:delta --help
+sf sgd:source:delta -a 53 --to HEAD --from origin/$BASE_BRANCH --output . -i manifest/mdapi/.sgdignore
 echo "--- package.xml generated with added and modified metadata since $LAST_DEPLOYED_COMMIT in Master ---"
 cat package/package.xml
 echo
