51f404eab68af7b7aff07597f4d9defed12f23a3
Shubham Kumar <shubhamsingh98354@gmail.com>
2023-05-04 23:14:59 +0530

    sync

--- .github/scripts/validateComponents.sh
+++ .github/scripts/validateComponents.sh
@@ -2,7 +2,7 @@ echo "Delta Check-Only"
 # Checking if any metadata changes are in package.xml
 if grep -q '<types>' package/package.xml;
 then
-    sf deploy metadata validate -a 56 -o $SOURCE_ORG_ALIAS -x package/package.xml -l RunSpecifiedTests -r $(cat ./testclass/testclass.txt) --verbose | tee ./DEPLOY_ORG.txt
+    sf deploy metadata -a 56 -o $SOURCE_ORG_ALIAS -x package/package.xml -l RunSpecifiedTests -t $(cat ./testclass/testclass.txt) --verbose --dry-run | tee ./DEPLOY_ORG.txt
     # sfdx force:source:deploy -c -u $SOURCE_ORG_ALIAS -x package/package.xml -l RunSpecifiedTests -r $(cat ./testclass/testclass.txt) --verbose | tee ./DEPLOY_ORG.txt
     DEPLOY_EXIT_CODE=${PIPESTATUS[0]}
     if [ $DEPLOY_EXIT_CODE != 0 ]
