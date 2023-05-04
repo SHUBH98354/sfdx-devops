fbc8876c1b9077c52e1f50b2b9b96f0529918978
Shubham Kumar <shubhamsingh98354@gmail.com>
2023-05-05 00:29:39 +0530

    sync

--- .github/scripts/validateComponents.sh
+++ .github/scripts/validateComponents.sh
@@ -3,7 +3,7 @@ echo "Delta Check-Only"
 if grep -q '<types>' package/package.xml;
 then
     sfdx update
-    sf deploy metadata -a 56 -o $SOURCE_ORG_ALIAS -x package/package.xml -l RunSpecifiedTests -t $(cat ./testclass/testclass.txt) --verbose --dry-run | tee ./DEPLOY_ORG.txt
+    sf deploy metadata -a 54.0 -o $SOURCE_ORG_ALIAS -x package/package.xml -l RunSpecifiedTests -t $(cat ./testclass/testclass.txt) --verbose --dry-run | tee ./DEPLOY_ORG.txt
     # sfdx force:source:deploy -c -u $SOURCE_ORG_ALIAS -x package/package.xml -l RunSpecifiedTests -r $(cat ./testclass/testclass.txt) --verbose | tee ./DEPLOY_ORG.txt
     DEPLOY_EXIT_CODE=${PIPESTATUS[0]}
     if [ $DEPLOY_EXIT_CODE != 0 ]
