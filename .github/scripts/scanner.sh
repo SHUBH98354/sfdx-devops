1e372f570ada4c9f0e2f976bc1deddd9b8fbd7a4
Shubham Kumar <shubhamsingh98354@gmail.com>
2023-05-05 00:26:35 +0530

    sync

--- .github/scripts/scanner.sh
+++ .github/scripts/scanner.sh
@@ -1,3 +1,3 @@
-echo y | sfdx plugins:install @salesforce/sfdx-scanner
-sfdx scanner --help
-sfdx scanner:run --format table --target force-app/main --normalize-severity -c "Security,Best Practices,Performance,Code Style,Design,Error Prone,ECMAScript 6,Possible Errors,Variables,Stylistic Issues"
+echo y | sf plugins install @salesforce/sfdx-scanner
+sf scanner --help
+sf scanner:run --format table --target force-app/main --normalize-severity -c "Security,Best Practices,Performance,Code Style,Design,Error Prone,ECMAScript 6,Possible Errors,Variables,Stylistic Issues"
